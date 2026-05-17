af90ca6ba7e70b3dab9dd2d624a19bbad93484b9
Matt Hoogcarspel <matthoogcarspel@Air-van-Matt.home>
2026-05-17 17:04:49 +0200

    fix: remove comment with special char causing Hermes crash

--- App.tsx
+++ App.tsx
@@ -9591,7 +9591,6 @@ const handleSave = async () => {
             ))}
           </View>
 
-          {/* Open Spot Chat knop - onder de GOING card, alleen native */}
           <Pressable
             onPress={() => {
               if (selectedSpot) {
